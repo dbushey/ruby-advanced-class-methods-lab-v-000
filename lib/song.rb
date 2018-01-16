51e4c1cc1e3541350ee59dee5b00d8f22ea3ab05
Daiane Bushey <daianebushey@gmail.com>
2018-01-16 21:05:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,11 +37,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     #song = self.create_by_name(name) unless self.find_by_name(name).include?(self.name)
-    if self.find_by_name(self.name) === nil
-      binding.pry
-      self.create_by_name(name)
-    end
-      self
+    self.create_by_name(name) || self.find_by_name(name)
   end
 
 
